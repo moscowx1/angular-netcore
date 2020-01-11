47fb818969ef678a8f18553ad2d20fd2cb501630
noName <moscowx1@mail.ru>
2020-01-11 15:51:34 +0500

    added post methods

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -1,19 +1,26 @@
 import { Product } from "./product.model";
+import { Supplier } from "./supplier.model";
+
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Filter } from "./configClasses.repository";
 
 const productsUrl = "/api/products";
+const suppliersUrl = "/api/suppliers";
 
 @Injectable()
 export class Repository {
 
   product: Product;
   products: Product[];
+  suppliers: Supplier[] = [];
   filter: Filter = new Filter();
+
+
   constructor(private http: HttpClient) {
-    this.filter.category = "soccer";
+    //this.filter.category = "soccer";
     this.filter.related = true;
+
     this.getProducts(true);
   }
 
@@ -35,5 +42,42 @@ export class Repository {
 
     this.http.get<Product[]>(url)
       .subscribe(prods => this.products = prods);
+  }
+
+  getSuppliers() {
+    this.http.get<Supplier[]>(suppliersUrl)
+      .subscribe(s => this.suppliers = s);
+  }
+
+  createProduct(p: Product) {
+    let data = {
+      name: p.name, category: p.category,
+      description: p.description, price: p.price,
+      supplier: p.supplier ? p.supplier.supplierId : 0
+    };
+
+    this.http.post<number>(productsUrl, data)
+      .subscribe(id => {
+        p.productId = id;
+        this.products.push(p);
+      });
+  }
+
+  createProductAndSupplier(p: Product, s: Supplier) {
+    let data = {
+      name: s.name, city: s.city, state: s.state
+    };
+
+
+    this.http.post<number>(suppliersUrl, data)
+      .subscribe(id => {
+        s.supplierId = id;
+        p.supplier = s;
+        console.log(s);
+        this.suppliers.push(s);
+        if (p != null) {
+          this.createProduct(p);
+        }
+      });
   }
 }

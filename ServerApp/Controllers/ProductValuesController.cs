3af4db4823fb43a3bc5b585705328aaa22535eaa
noName <moscowx1@mail.ru>
2020-01-11 14:53:48 +0500

    added product post method

--- ServerApp/Controllers/ProductValuesController.cs
+++ ServerApp/Controllers/ProductValuesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ServerApp.Models;
+using ServerApp.Models.BindingTargets;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -82,6 +83,24 @@ namespace ServerApp.Controllers
             {
                 return query;
             }
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] ProductData pdata)
+        {
+            if (ModelState.IsValid)
+            {
+                Product p = pdata.Product;
+
+                if (p.Supplier != null && p.Supplier.SupplierId != 0)
+                    context.Attach(p.Supplier);
+
+                context.Add(p);
+                context.SaveChanges();
+                return Ok(p.ProductId);
+            }
+            else
+                return BadRequest(ModelState);
         }
     }
 }

ada0a27a9eae5bf06b65e3df8f645b367d6a0b5b
noName <moscowx1@mail.ru>
2020-01-11 12:06:04 +0500

    f

---
+++ ClientApp/src/app/models/repository.ts
@@ -0,0 +1,26 @@
+import { Product } from "./product.model";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable()
+export class Repository {
+
+  productData: Product;
+
+  constructor(private http: HttpClient) {
+    this.getProduct(1);
+  }
+
+  getProduct(id: number) {
+    this.http.get<Product>("/api/products/" + id)
+      .subscribe(p => {
+        this.productData = p;
+        console.log("Product Data Received");
+      });
+  }
+
+  get product(): Product {
+    console.log("Product Data Requested");
+    return this.productData;
+  }
+}
---
+++ ServerApp/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using ServerApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServerApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(connectionString));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "../ClientApp";
+                spa.UseAngularCliServer("start");
+            });
+
+            SeedData.SeedDatabase(services.GetRequiredService<DataContext>());
+        }
+    }
+}

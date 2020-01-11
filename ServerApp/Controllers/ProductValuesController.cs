318733eaf03e98e3009183ba3486755fd6fb237c
noName <moscowx1@mail.ru>
2020-01-11 12:38:17 +0500

    fix circular ref

--- ServerApp/Controllers/ProductValuesController.cs
+++ ServerApp/Controllers/ProductValuesController.cs
@@ -19,11 +19,21 @@ namespace ServerApp.Controllers
         [HttpGet("{id}")]
         public Product GetProduct(long id)
         {
-            System.Threading.Thread.Sleep(500);
-            return context.Products
+            Product res = context.Products
                 .Include(p => p.Supplier)
                 .Include(p => p.Ratings)
                 .FirstOrDefault(p => p.ProductId == id);
+
+            if(res != null)
+            {
+                if (res.Supplier != null)
+                    res.Supplier.Products = null;
+
+                if (res.Ratings != null)
+                    foreach (Rating r in res.Ratings)
+                        r.Product = null;
+            }
+            return res;
         }
     }
 }

097c53e7bf13ea8791fe057ea562378a7856df25
noName <moscowx1@mail.ru>
2020-01-11 12:50:15 +0500

    added circular ref in additional data

--- ServerApp/Controllers/ProductValuesController.cs
+++ ServerApp/Controllers/ProductValuesController.cs
@@ -20,14 +20,22 @@ namespace ServerApp.Controllers
         public Product GetProduct(long id)
         {
             Product res = context.Products
-                .Include(p => p.Supplier)
+                .Include(p => p.Supplier).ThenInclude(s => s.Products)
                 .Include(p => p.Ratings)
                 .FirstOrDefault(p => p.ProductId == id);
 
             if(res != null)
             {
                 if (res.Supplier != null)
-                    res.Supplier.Products = null;
+                    res.Supplier.Products = res.Supplier.Products.Select(p =>
+                        new Product
+                        {
+                            ProductId = p.ProductId,
+                            Name = p.Name,
+                            Category = p.Category,
+                            Description = p.Description,
+                            Price = p.Price
+                        });
 
                 if (res.Ratings != null)
                     foreach (Rating r in res.Ratings)
